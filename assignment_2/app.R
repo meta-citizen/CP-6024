351222930216f070034df8f0713e0802f668a223
NullSpace <lancaster.zachary@gmail.com>
2017-03-06 21:54:05 -0500

    Updated Files to Current Version

--- assignment_2/app.R
+++ assignment_2/app.R
@@ -2,6 +2,8 @@ library(ggplot2)
 library(shiny)
 
 #Set Environment
+
+
 ga_counties <- read.csv('a01-gareis2003.txt')
 ga_proj <- read.csv('a40-gaprojected.txt')
 
@@ -11,35 +13,35 @@ analysis_years <- data.frame(unique(ga_counties$YEAR))
 
 #
 ui <- fluidPage(
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(inputId = "base_year",
-                  label = "Analysis Range",
-                  value = c(1969,2003), min = 1969, max = 2003),
-      selectInput(inputId = "a_level",
-                  label = "Analysis Level",
-                  choices = analysis_level$AREANAME,
-                  selected = "Appling, Georgia [13001]",
-                  multiple = FALSE),
-      selectInput(inputId = "a_type",
-                  label = "Analysis Type",
-                  choices = analysis_type$LINETITLE),
-      selectInput(inputId = "proj_year", 
-                  label = "Projection Year",
-                  choices = c("2010" = 2010, 
-                              "2040" = 2040),
-                  selected = 2010)
-      ),
-    mainPanel(
-      plotOutput(outputId = "yv_scatter")
-
-    )
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput(inputId = "base_year",
+                    label = "Analysis Range",
+                    value = c(1969,2003), min = 1969, max = 2003),
+        selectInput(inputId = "a_level",
+                    label = "Analysis Level",
+                    choices = analysis_level$AREANAME,
+                    selected = "Appling, Georgia [13001]",
+                    multiple = FALSE),
+        selectInput(inputId = "a_type",
+                    label = "Analysis Type",
+                    choices = analysis_type$LINETITLE),
+        selectInput(inputId = "proj_year", 
+                    label = "Projection Year",
+                    choices = c("2010",
+                                "2040"),
+                    selected = "2010"),
+        selectInput(input = "proj_line", label = "Projection Line",
+                    choices = c("Simple Linear", "Simple Geometric"))
+        ),
+      mainPanel(
+        plotOutput(outputId = "yv_scatter")
+      )
   ),
-  dataTableOutput(outputId = "proj_table"),  
-  dataTableOutput(outputId = "pop_table")
-  
+    dataTableOutput(outputId = "proj_table"),  
+    dataTableOutput(outputId = "pop_table")
+  )
 
-)
 
 server <- function(input, output) {
   source('proj_src.R')
@@ -50,7 +52,7 @@ server <- function(input, output) {
     plot(
         x = p_x,
         y = p_y,
-        xlim = range(input$base_year:input$base_year[2]),
+        xlim = range(input$base_year[1]:input$base_year[2]),
         ylim = range(min(p_y):max(p_y)),
         xlab = "Year",
         ylab = input$a_type)})
@@ -59,6 +61,7 @@ server <- function(input, output) {
   }, options = list(lengthMenu = c(5,10,15), pageLength = 5))
   output$proj_table = renderDataTable({
 
+    if (input$proj_year == "2010") { var <- 2010 } else {var <- 2040 }
     #Establish i and j table frames
     i_base <- subset(ga_counties, AREANAME == input$a_level & LINETITLE == input$a_type & YEAR == input$base_year[1])
     i_launch <- subset(ga_counties, AREANAME == input$a_level & LINETITLE == input$a_type & YEAR == input$base_year[2])
@@ -67,18 +70,36 @@ server <- function(input, output) {
     j_launch <- subset(ga_counties, LINECODE == 20 & YEAR == input$base_year[1] & FIPS == 13000)
     j_proj <- subset(ga_proj, DataType == 'Population' & Year == input$proj_year)
     
-    tmp <- data.frame(smp_linear(i_base$VALUE, 
+    tmp <- data.frame(rbind(smp_linear(i_base$VALUE, 
                                  i_launch$VALUE, 
                                  input$base_year[2]-input$base_year[1], 
-                                 is.integer(input$proj_year)-input$base_year[2])
+                                 var-input$base_year[2]),
+                      smp_geom(i_base$VALUE, 
+                               i_launch$VALUE, 
+                               input$base_year[2]-input$base_year[1], 
+                               var-input$base_year[2]),
+                      smp_exp(i_base$VALUE, 
+                              i_launch$VALUE, 
+                              input$base_year[2]-input$base_year[1], 
+                              var-input$base_year[2]),
+                      mod_exp(i_base$VALUE,
+                              i_launch$VALUE,
+                              input$base_year[2]-input$base_year[1],
+                              var-input$base_year[2]),
+                      logistic(i_base$VALUE, 
+                               i_launch$VALUE, 
+                               input$base_year[2]-input$base_year[1], 
+                               input$proj_year-input$base_year[2])
                                 )
-                      
+                      )
     
     out_tab <- data.frame(PROJECTION_TYPE = c("Simple Linear","Simple Geometric", "Simple Exponential", "Line Fit", "Geometric",
-                                "Modified Exponential", "Logistic", "Constant Share", "Shift Share", "Growth Share"),
+                                              "Modified Exponential", "Logistic", "Constant Share", "Shift Share", "Growth Share"),
                           PROJECTION = tmp$population,
                           RATE = tmp$rate
     )
+    
+
   })
 }
 
--- assignment_2/proj_src.R
+++ assignment_2/proj_src.R
@@ -17,8 +17,8 @@ smp_geom <- function(pop_base, pop_launch, years, proj_years) {
   return(data.frame(population = pp, rate = rate))
 }
 
-#Simple Exp
-smp_exp <- function(pop_base, pop_launch, years, proj_years) {
+#Geometric
+geometric <- function(pop_base, pop_launch, years, proj_years) {
   rate <- ((pop_launch/pop_base)^(1/years))-1
   pp <- pop_launch*(1+rate)^proj_years
   
@@ -30,12 +30,31 @@ line_proj <- function() {
   supsmu() #projection function
 }
 
-#Geometric 
+#Simple Exponential 
+smp_exp <- function(pop_base, pop_launch, years, proj_years) {
+  rate <- log(pop_launch/pop_base)*(1/years)
+  pp <- pop_launch * exp(rate*proj_years)
+  
+  return(data.frame(population = pp, rate = rate))
+}
 #Modified Exp
-mod_exp <- function(pop_launch, years, proj_years, pop_time, rate) {
-  return(population = pp, rate = rate)
+mod_exp <- function(pop_base, pop_launch, years, proj_years, pop_time) {
+    rate <- log(pop_launch/pop_base)*(1/years)
+    pp <- (1.5*pop_launch)+pop_launch*rate^(proj_years)
+    
+    return(data.frame(population = pp, rate = rate))
 }
 #Logistic
+#p_t = p_l/(1+r(e^-limit*x))
+
+logistic <- function(pop_base, pop_launch, years, proj_years) {
+  a = 1.5*pop_launch
+  b = years
+  rate = (log((1.5*pop_base)/pop_launch)-1)/b
+  pp = a/(1+exp(-rate*years))
+  
+  return(data.frame(population = pp, rate = rate))
+}
 #Constant Share
 c_share <- function(pop_j_t, pop_j_l, pop_i_l) {
   pp = (pop_i_l/pop_j_l)*pop_j_t
